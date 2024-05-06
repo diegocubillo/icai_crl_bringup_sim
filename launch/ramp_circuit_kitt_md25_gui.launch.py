75223769021aa2e09be1896f4f7b6b4d5e01b145
diegocubillo <dcubillo@comillas.edu>
2024-05-06 19:13:13 +0200

    Leave only launch files for md25 plugin  main version and remove unused launch files

--- launch/ramp_circuit_kitt_md25_gui.launch.py
+++ launch/ramp_circuit_kitt_md25_gui.launch.py
@@ -16,15 +16,16 @@ def generate_launch_description():
     # Setup project paths
     pkg_project_bringup = get_package_share_directory('icai_crl_bringup')
     pkg_project_gazebo = get_package_share_directory('icai_crl_gazebo')
+    pkg_project_description = get_package_share_directory('icai_crl_description')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     # Setup to launch the simulator and Gazebo world
-    world_sdf_path = os.path.join(pkg_project_gazebo, 'worlds', 'control_laboratory.sdf')
+    world_sdf_path = os.path.join(pkg_project_gazebo, 'worlds', 'empty_world.sdf')
     config_gui_path = os.path.join(pkg_project_bringup, 'config', 'gazebo_gui.config')
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': world_sdf_path + ' -v --gui-config ' + config_gui_path}.items(),
+        launch_arguments={'gz_args': world_sdf_path + ' --gui-config ' + config_gui_path}.items(),
     )
 
     # Bridge ROS topics and Gazebo messages for establishing communication
@@ -32,8 +33,20 @@ def generate_launch_description():
         package='ros_gz_bridge',
         executable='parameter_bridge',
         parameters=[{
-            'config_file': os.path.join(pkg_project_bringup, 'config', 'kitt_md25_bridge_o3.yaml'),
+            'config_file': os.path.join(pkg_project_bringup, 'config', 'kitt_md25_bridge.yaml'),
         }],
+        output='screen'
+    )
+
+
+    # Spawn the ramp circuit model in the Gazebo world
+    spawn_circuit = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-name', 'ramp_circuit',
+                   '-x', '0',
+                   '-y', '0',
+                   '-file', os.path.join(pkg_project_description, 'models', 'environments', 'ramp_circuit')],
         output='screen'
     )
 
@@ -41,17 +54,17 @@ def generate_launch_description():
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
-        arguments=['-name', 'kitt_md25_o3',
-                   '-x', '3',
-                   '-y', '-3',
-                   '-z', '0.1',
-                   '-Y', '3.1416',
-                   '-file', os.path.join(pkg_project_gazebo, 'models', 'md25_driver', 'kitt_md25_o3')],
+        arguments=['-name', 'kitt_md25',
+                   '-x', '0.5',
+                   '-y', '-0.65',
+                   '-z', '0.84',
+                   '-file', os.path.join(pkg_project_gazebo, 'models', 'md25_driver', 'kitt_md25')],
         output='screen'
     )
 
     return LaunchDescription([
         gz_sim,
         bridge,
+        spawn_circuit,
         spawn_entity
     ])
